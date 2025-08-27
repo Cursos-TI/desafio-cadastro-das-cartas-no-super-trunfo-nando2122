e037367fdc1089c171ff790b9bff15375ae6b89a
nando2122 <caitanofernando7@gmail.com>
2025-08-27 21:35:37 +0000

    Tentei identar colocando as duas cartas no mesmo comando de texto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,11 +10,11 @@ int main() {
 
     int pontos = 30;
     int populacao = 100000;
-    float area = 2.000000;
+    float area = 2.00000;
     float pib = 30.0000;
-    char estado[20] = "Ceara";
+    char estado[20] = "A";
     char cidade[20] = "Fortaleza";
-    char codigo [20]= "C01";
+    char codigo [20]= "A01";
 
     printf("Digite quantos pontos turísticos tem: \n");
     scanf("%d", &pontos);
@@ -29,14 +29,46 @@ int main() {
     scanf("%f", &pib);
 
     printf("Digite o Estado: \n");
-    scanf("%s", &estado);
+    scanf("%s", estado);
 
     printf("Digite a cidade: \n");
-    scanf("%s", &cidade);
+    scanf("%s", cidade);
 
     printf("Digite o código da carta: \n");
-    scanf("%s", &codigo);
+    scanf("%s", codigo);
 
+}
+
+    int main() {
+
+    int pontos = 20;
+    int populacao = 200000;
+    float area = 1.00000;
+    float pib = 33.0000;
+    char estado[20] = "B";
+    char cidade[20] = "Rio de Janeiro";
+    char codigo [20]= "B01";
+
+    printf("Digite quantos pontos turísticos tem: \n");
+    scanf("%d", &pontos);
+
+    printf("Digite o número da população: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área em km²: \n");
+    scanf("%f", &area);
+
+    printf("Digite o número do PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o estado: \n");
+    scanf("%s", estado);
+
+    printf("Digite a cidade: \n");
+    scanf("%s", cidade);
+
+    printf("Digite o código: \n");
+    scanf("%s", codigo);
 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
